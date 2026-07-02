81d170eb1a5c1c8ca7077747f33ff36f41ec4708
nurenibabatundelawal <nurenibabatundelawal@gmail.com>
2026-07-02 11:07:36 -0700

    Updated db_connect

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 $host = "bmvog3d4wn5nn6u1i36m-mysql.services.clever-cloud.com";
 $user = "uhu2jnt6yksmwxim";
-$password = "university_attendance_system";
+$password = "8bldiQltolSkk7VN4Nh5";
 $database = "bmvog3d4wn5nn6u1i36m";
 $port = 3306;
 
